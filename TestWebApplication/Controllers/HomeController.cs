44072e9e26075849b4d9191f5bcf41794ce9a426
ekanki <esingh951@gmail.com>
2021-08-22 08:44:36 +0530

    Added a method to calculate sum of two given numbers.

--- TestWebApplication/Controllers/HomeController.cs
+++ TestWebApplication/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestWebApplication.Models;
 
 namespace TestWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        Functionality funObj = null;
+        public HomeController()
+        {
+            funObj = new Functionality();
+        }
         public ActionResult Index()
         {
+            ViewBag.Sum = funObj.Sum(10, 15);
             return View();
         }
 
